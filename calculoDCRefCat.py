070fe91f9693f45e686765cd2d109271ef85d6be
PaulinaLP <77441300+PaulinaLP@users.noreply.github.com>
2023-07-21 10:01:18 +0200

    Update calculoDCRefCat.py
    correccion error mapeo

--- calculoDCRefCat.py
+++ calculoDCRefCat.py
@@ -7,7 +7,7 @@ def calcularDigitoControl(referencia):
     
     # Paso 2: Convertir las letras a números según el mapeo
     conversion_table = {
-        'A': 1, 'B': 20, 'C': 3, 'D': 14, 'E': 4, 'F': 6, 'G': 7, 'H': 8, 'I': 9,
+        'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9,
         'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'Ñ': 15, 'O': 16, 'P': 17,
         'Q': 18, 'R': 19, 'S': 20, 'T': 21, 'U': 22, 'V': 23, 'W': 24, 'X': 25,
         'Y': 26, 'Z': 27
